aa470595bae3b2f566356aa0475ed567f7cdf4c7
Rik <rikwork8515@gmail.com>
2016-08-08 11:40:12 +0300

    return

---
+++ src/main/java/Frontend.java
@@ -0,0 +1,23 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class Frontend extends HttpServlet {
+    private String login = "";
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws IOException {
+        System.out.println("MyServlet is opened");  // print in console
+        response.getWriter().println("MyServlet say to you Hello!");    //print on the page
+        response.getWriter().println("You typed such name =" + request.getParameter("name"));
+
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response){
+
+    }
+}
